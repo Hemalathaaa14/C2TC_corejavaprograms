31dce6872170a2b61346e98470cc369674e02e53
hemalatha M <hemalathamuniyan@gmail.com>
2025-08-05 08:55:11 +0530

    initial commit

---
+++ src/day1_basic/SampleDemo.java
@@ -0,0 +1,10 @@
+package day1_basic;
+
+public class SampleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my basic code");
+	}
+
+}
