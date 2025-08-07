a88a6f16b16e9757d632904306c0e5c6ddc1c3c2
hemalatha M <hemalathamuniyan@gmail.com>
2025-08-07 20:39:00 +0530

    this is java project contains basic java programs

---
+++ src/com/tnsif/Day1/Firstprogram.java
@@ -0,0 +1,8 @@
+//Demonstration of simple java program
+package com.tnsif.Day1;
+
+public class Firstprogram {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
