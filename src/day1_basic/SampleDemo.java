1f813df731724a2051500217fc6377d5edbda939
hemalatha M <hemalathamuniyan@gmail.com>
2025-08-17 20:30:01 +0530

    updated

--- src/day1_basic/SampleDemo.java
+++ src/day1_basic/SampleDemo.java
@@ -1,10 +1,17 @@
 package day1_basic;
 
 public class SampleDemo {
+    public static void main(String[] args) {
+        student s = new student();
+        s.setSid(301);
+        s.setSname("Dia");
+        s.setAvg(85.6f);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("this is my basic code");
-	}
-
+        System.out.println("Student id:" + s.getSid());
+        System.out.println("Student Name:" + s.getSname());
+        System.out.println("Student Average:" + s.getAvg());
+        
+        System.out.println(s.toString());
+    }
 }
+
