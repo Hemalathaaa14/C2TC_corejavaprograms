aa35c380c3a2c6fffd859a3496d53754687b5014
hemalatha M <hemalathamuniyan@gmail.com>
2025-08-20 14:16:25 +0530

    updated

---
+++ src/abstract_example/TnsifCgDemo.java
@@ -0,0 +1,18 @@
+package abstract_example;
+
+public class TnsifCgDemo {
+
+	public static void main(String[] args) 
+	{
+		Trainer t=new TechTrainer();
+		
+		t.session();
+		t.checks();
+		
+		t=new SoftSkillTrainer();
+		
+		t.session();
+		t.checks();
+	}
+}
+
